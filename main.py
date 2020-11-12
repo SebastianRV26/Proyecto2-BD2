f6033fab2dfeb0ed75b1536fdda20a4730a8a597
Sebastian <sebasrv26@estudiantec.cr>
2020-11-11 20:34:05 -0600

    Show table privileges and attributes

--- main.py
+++ main.py
@@ -2,12 +2,46 @@ from tkinter import *
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import Combobox
+from tkinter import ttk
 import os
 import psycopg2
 import json
 
+class Table(ttk.Frame):
+    
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Privilegios de la tabla y sus atributos")
+        
+        self.treeview2 = ttk.Treeview(self, columns=("select", "insert", "update", "delete", "references", "trigger", "truncate"))
+        self.treeview2.pack()
+        self.treeview2.heading("#0", text="Atributo")
+        self.treeview2.heading("select", text="Select")
+        self.treeview2.heading("insert", text="Insert")
+        self.treeview2.heading("update", text="Update")
+        self.treeview2.heading("delete", text="Delete")
+        self.treeview2.heading("references", text="References")
+        self.treeview2.heading("trigger", text="Trigger")
+        self.treeview2.heading("truncate", text="truncate")
+
+        self.treeview = ttk.Treeview(self, columns=("select", "insert", "update", "references"))
+        self.treeview.pack()
+        self.treeview.heading("#0", text="Atributo")
+        self.treeview.heading("select", text="Select")
+        self.treeview.heading("insert", text="Insert")
+        self.treeview.heading("update", text="Update")
+        self.treeview.heading("references", text="References")
+        
+        self.pack()
+
+    def set_rowTable(self, name, select, insert, update, delete, references, trigger, truncate):
+        self.treeview2.insert("", tk.END, text=name, values=(select, insert, update, delete, references, trigger, truncate))
+
+    def set_row(self, name, select, insert, update, references):
+        self.treeview.insert("", tk.END, text=name, values=(select, insert, update, references))
+
 # global variables
-user = ""
+globalUser = ""
 
 # Cambiar de ventana.
 def toBack (currentWindow, nextWindow):
@@ -17,7 +51,8 @@ def toBack (currentWindow, nextWindow):
 # Crear conección.
 def createConnection(user, password, server, port, db):
     # user = user
-    user = "basesII" # delete later
+    global globalUser
+    globalUser = "basesII" # delete later
     try:
         # Está quemado, cambiarlo por los parametros
         conn = psycopg2.connect(user = "basesII",
@@ -74,28 +109,45 @@ def privilegeWindow(root, connection):
 
     cmbTables["values"] = tuple(columns)
 
-    Label (window, text="Atributos", font="Arial, 12").pack()
-    cmbAttributes = Combobox(window, font="Arial, 12")
-    cmbAttributes.pack()
-    
     def callback(eventObject):
         x = cmbTables.get()
-        item = x
-        items = item.split('.')
-        # Get Attributes
-        cursor.execute("""SELECT column_name
-        FROM information_schema.columns
-        WHERE "table_name"='""" + items[1] + """' 
-        AND table_schema='""" + items[0] + """'""")
-        result = cursor.fetchall() 
-        columns = []
+        items = x.split('.')
+
+        window2 = Tk()
+        table = Table(window2)
+        
+        cursor.execute("""select 
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'select') as select,
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'insert') as insert,
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'update') as update,
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'delete') as delete, 
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'references') as references,
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'trigger') as trigger,
+            HAS_TABLE_PRIVILEGE('"""+globalUser+"""', '"""+items[1]+"""', 'truncate') as truncate
+        from INFORMATION_SCHEMA.TABLES T
+        WHERE T.TABLE_NAME='"""+ items[1] +"""' 
+        AND T.table_schema = '""" + items[0] + """'""")
+        i = cursor.fetchall()[0]
+
+        table.set_rowTable(items[1] , str(i[0]), str(i[1]), str(i[2]), str(i[3]), str(i[4]), str(i[5]), str(i[6]))
+
+        cursor.execute("""select COLUMN_NAME, 
+            has_column_privilege('"""+globalUser+"""', '"""+items[1]+"""', COLUMN_NAME, 'select') as select,
+            has_column_privilege('"""+globalUser+"""', '"""+items[1]+"""', COLUMN_NAME, 'insert') as insert,
+            has_column_privilege('"""+globalUser+"""', '"""+items[1]+"""', COLUMN_NAME, 'update') as update,
+            has_column_privilege('"""+globalUser+"""', '"""+items[1]+"""', COLUMN_NAME, 'references') as references
+        from INFORMATION_SCHEMA.COLUMNS C
+        WHERE C.TABLE_NAME='"""+ items[1] +"""' 
+        AND C.table_schema = '""" + items[0] + """'""")
+
+        result = cursor.fetchall()         
         for i in result:
-            columns.append(i[0])
-        cmbAttributes["values"] = tuple(columns)
-        cmbAttributes.current(0)
+            # columns.append(i[0])
+            table.set_row(i[0], str(i[1]), str(i[2]), str(i[3]), str(i[4]))
+
+        table.mainloop()
 
     cmbTables.bind("<<ComboboxSelected>>", callback) # on item clicked
-
     btn_destroy = Button(window, text="Atrás", font="Arial, 12", command = lambda:toBack(window, root))
     btn_destroy.pack()
 
